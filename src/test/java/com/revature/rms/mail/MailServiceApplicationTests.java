4d9113b310b6fbede37b2ffc829755d5e07cc5c2
wsingleton <wezley.singleton@gmail.com>
2020-03-02 20:56:55 -0500

    init repo and project

---
+++ src/test/java/com/revature/rms/mail/MailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.rms.mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
